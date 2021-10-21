b7a7023c53290a72253ab46afd5a8785600abbce
Fredrik Blixth <fredrik.blixth@gmail.com>
2021-10-21 22:29:01 +0200

    Project setup.
    tsconfig.json, package.json, .gitignore

---
+++ .eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'airbnb-typescript/base',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: './tsconfig.eslint.json',
+  },
+  plugins: [
+    '@typescript-eslint',
+  ],
+  rules: {
+    'import/prefer-default-export': 'off'
+  },
+};
